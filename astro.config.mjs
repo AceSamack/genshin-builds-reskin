8ab1ac6b2786031277ff714d45d6dd84b5d35626
Akhala <lolipoploplo@gmail.com>
2026-05-11 23:26:05 +0200

    deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://akhalaplo.github.io',
+  base: '/genshin-builds',
   image: {
     domains: ['wiki.hoyolab.com', 'upload-static.hoyoverse.com'],
   },
