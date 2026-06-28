2663a7bb2c094edef62bd6ae21acce5881fa92be
AceSamack <louis.leleu0@gmail.com>
2026-06-28 22:45:38 +0200

    fix/deployment issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,12 @@ import { defineConfig } from 'astro/config';
 const isVercel = process.env.VERCEL === '1';
 
 export default defineConfig({
-  site:
-    isVercel && process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}`
-      : 'https://acesamack.github.io',
-  base: isVercel ? '/' : '/genshin-builds',
-  image: {
-    domains: ['wiki.hoyolab.com', 'upload-static.hoyoverse.com'],
-  },
+    site:
+        isVercel && process.env.VERCEL_URL
+            ? `https://${process.env.VERCEL_URL}`
+            : 'https://acesamack.github.io',
+    base: isVercel ? '/' : '/genshin-builds-reskin',
+    image: {
+        domains: ['wiki.hoyolab.com', 'upload-static.hoyoverse.com'],
+    },
 });
