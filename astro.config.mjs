b6a46f6a5600882c391fe109ed4bc93c9893d01c
AceSamack <louis.leleu0@gmail.com>
2026-06-28 22:37:38 +0200

    fix/astro-config url

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ export default defineConfig({
   site:
     isVercel && process.env.VERCEL_URL
       ? `https://${process.env.VERCEL_URL}`
-      : 'https://akhalaplo.github.io',
+      : 'https://acesamack.github.io',
   base: isVercel ? '/' : '/genshin-builds',
   image: {
     domains: ['wiki.hoyolab.com', 'upload-static.hoyoverse.com'],
